db5ec2a06db31baef068f59ccdcd7cb3e6122c6a
Vinayagem <39568563+Vinayagem@users.noreply.github.com>
2019-11-19 02:21:21 +0400

    Add files via upload

---
+++ www/js/index.js
@@ -0,0 +1,112 @@
+
+//login
+function login() {
+        var email = document.getElementById('txtemail').value;
+
+        var password = document.getElementById('txtpassword').value;
+
+        if (email.trim() == "" || password.trim() == "") {
+          $.alert({
+            title: 'Empty!',
+            content: 'Either Email or Password is not filled. Please check and try again.',
+            closeIcon: true
+          });
+        } else {
+
+
+          if (ValidateEmail(email) == true) {
+            var getresultsurl = "http://localhost/coursework/login.php?email=" + email + "&password=" + password;
+            $.get(getresultsurl, function(data, status) {
+
+              if (data == "Success") {
+                location.href = '#dashboard';
+              } else {
+                $.alert({
+                  title: 'Error!',
+                  content: data,
+                  closeIcon: true
+                });
+              }
+            });
+          } else {
+            $.alert({
+              title: 'Error!',
+              content: "Email is not valid. Please enter a good one and try again.",
+              closeIcon: true
+            });
+          }
+        }
+      }
+
+      function check() {
+        var getresultsurl = "http://localhost/coursework/login.php?operation=" + 11;
+        $.get(getresultsurl, function(data, status) {
+          if (data == "Exist") {
+            location.href = '#dashboard';
+          } else {}
+        });
+      }
+
+      //Email Validation
+      function ValidateEmail(mail) {
+        if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail)) {
+          return true;
+        }
+        return false;
+      }
+
+
+// dashboard
+      $(window).load(function() {
+      $('.flexslider').flexslider({
+        animation: "slide"
+      });
+    });
+
+    function myFunction() {
+     var x = document.getElementById("myLinks");
+     if (x.style.display === "block") {
+       x.style.display = "none";
+     } else {
+       x.style.display = "block";
+     }
+    }
+
+//admin adminlogin
+function adminlogin() {
+        var email = document.getElementById('txtemail').value;
+
+        var password = document.getElementById('txtpassword').value;
+
+        if (email.trim() == "" || password.trim() == "") {
+          $.alert({
+            title: 'Empty!',
+            content: 'Either Email or Password is not filled. Please check and try again.',
+            closeIcon: true
+          });
+        } else {
+
+
+          if (ValidateEmail(email) == true) {
+            var getresultsurl = "http://localhost/coursework/login.php?email=" + email + "&password=" + password;
+            $.get(getresultsurl, function(data, status) {
+
+              if (data == "Success") {
+                location.href = '#admindashboard';
+              } else {
+                $.alert({
+                  title: 'Error!',
+                  content: data,
+                  closeIcon: true
+                });
+              }
+            });
+          } else {
+            $.alert({
+              title: 'Error!',
+              content: "Email is not valid. Please enter a good one and try again.",
+              closeIcon: true
+            });
+          }
+        }
+      }
